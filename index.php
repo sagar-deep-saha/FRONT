7e025333e5cf89a02cf038e43e6597ba19183db3
Ritesh <sagarsaha@outlook.in>
2024-04-21 02:32:09 +0530

    Change done in select system

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<code>Changes In The True Section and Crossover all the semester parts</code>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Front</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
@@ -46,93 +45,94 @@
 
 
             <select class="form-select" aria-label="Default select example" name="subject" id="alternate3">
-                <option value="1">Effective Technical Communication </option>
-                <option value="2">Mathematics-III</option>
-                <option value="3">Biology for Engineers </option>
-                <option value="4"> Engineering Mechanics</option>
-                <option value="5"> Digital Logic & Microprocessor. </option>
-                <option value="6"> Data Structure & Algorithm </option>
-                <option value="7"> Java Programming Lab </option>
-                <option value="8"> Data Structure Lab </option>
-                <option value="9">Digital Electronics & Microprocessor Lab </option>
-                <option value="10">Indian Constitution </option>
+                <option class="" value="" hidden>Select Subject</option>
+                <option class="porn3" value="1">Effective Technical Communication </option>
+                <option class="porn3" value="2">Mathematics-III</option>
+                <option class="porn3" value="3">Biology for Engineers </option>
+                <option class="porn3" value="4">Engineering Mechanics</option>
+                <option class="porn3" value="5">Digital Logic & Microprocessor. </option>
+                <option class="porn3" value="6">Data Structure & Algorithm </option>
+                <option class="porn3" value="7">Java Programming Lab </option>
+                <option class="porn3" value="8">Data Structure Lab </option>
+                <option class="porn3" value="9">Digital Electronics & Microprocessor Lab </option>
+                <option class="porn3" value="10">Indian Constitution </option>
 
-            </select>
+            <!-- </select> -->
 
-            <select class="form-select" aria-label="Default select example" name="subject" id="alternate4">
-                <option value="1">Engineering Economics and Accountancy</option>
-                <option value="2">Universal Human Values-II</option>
-                <option value="3">Discrete Mathematics </option>
-                <option value="4">Computer Organization & Architecture </option>
-                <option value="5"> Operating Systems </option>
-                <option value="6">Object Oriented Programming </option>
-                <option value="7">IT Workshop (Python/R) </option>
-                <option value="8"> Operating System Lab </option>
-                <option value="9">Object Oriented Programming Lab </option>
-                <option value="10">Essence of Indian Knowledge Tradition </option>
-            </select>
+            <!-- <select class="form-select" aria-label="Default select example" name="subject" id="alternate4"> -->
+                <option class="porn4" value="1">Engineering Economics and Accountancy</option>
+                <option class="porn4" value="2">Universal Human Values-II</option>
+                <option class="porn4" value="3">Discrete Mathematics </option>
+                <option class="porn4" value="4">Computer Organization & Architecture </option>
+                <option class="porn4" value="5">Operating Systems </option>
+                <option class="porn4" value="6">Object Oriented Programming </option>
+                <option class="porn4" value="7">IT Workshop (Python/R) </option>
+                <option class="porn4" value="8"> Operating System Lab </option>
+                <option class="porn4" value="9">Object Oriented Programming Lab </option>
+                <option class="porn4" value="10">Essence of Indian Knowledge Tradition </option>
+            <!-- </select> -->
 
-            <select class="form-select" aria-label="Default select example" name="subject" id="alternate5">
-                <option value="1">Professional Practice, Law and Ethics </option>
-                <option value="2">Design and Analysis of Algorithm</option>
-                <option value="3">Database Management Systems </option>
-                <option value="4">Formal Language & Automata Theory</option>
-                <option value="5"> Artificial Intelligence </option>
-                <option value="6">Computer Networks </option>
-                <option value="7">Algorithm Lab </option>
-                <option value="8">Database Management System Lab</option>
-                <option value="9">Computer Hardware & Network Lab </option>
-                <option value="10">Industry Internship - I </option>
-            </select>
+            <!-- <select class="form-select" aria-label="Default select example" name="subject" id="alternate5"> -->
+                <option class="porn5" value="1">Professional Practice, Law and Ethics </option>
+                <option class="porn5" value="2">Design and Analysis of Algorithm</option>
+                <option class="porn5" value="3">Database Management Systems </option>
+                <option class="porn5" value="4">Formal Language & Automata Theory</option>
+                <option class="porn5" value="5">Artificial Intelligence </option>
+                <option class="porn5" value="6">Computer Networks </option>
+                <option class="porn5" value="7">Algorithm Lab </option>
+                <option class="porn5" value="8">Database Management System Lab</option>
+                <option class="porn5" value="9">Computer Hardware & Network Lab </option>
+                <option class="porn5" value="10">Industry Internship - I </option>
+            <!-- </select> -->
 
-            <select class="form-select" aria-label="Default select example" name="subject" id="alternate6">
-                <option value="1">Digital Image Processing</option>
-                <option value="2">Compiler Design </option>
-                <option value="3">Cryptography and Network Security </option>
-                <option value="4">Software Engineering </option>
-                <option value="5"> Advanced Java Lab </option>
-                <option value="6">Web Technology Lab </option>
-                <option value="7">Image Processing Lab </option>
-                <option value="8">Advanced Computer Architecture</option>
-                <option value="9">Data Mining</option>
-                <option value="10">Web Technology</option>
+            <!-- <select class="form-select" aria-label="Default select example" name="subject" id="alternate6"> -->
+                <option class="porn6" value="1">Digital Image Processing</option>
+                <option class="porn6" value="2">Compiler Design </option>
+                <option class="porn6" value="3">Cryptography and Network Security </option>
+                <option class="porn6" value="4">Software Engineering </option>
+                <option class="porn6" value="5">Advanced Java Lab </option>
+                <option class="porn6" value="6">Web Technology Lab </option>
+                <option class="porn6" value="7">Image Processing Lab </option>
+                <option class="porn6" value="8">Advanced Computer Architecture</option>
+                <option class="porn6" value="9">Data Mining</option>
+                <option class="porn6" value="10">Web Technology</option>
             </select>
         </div>
 
 
         <script>
             $(document).ready(function() {
-                $("#alternate3").hide();
-                $("#alternate4").hide();
-                $("#alternate5").hide();
-                $("#alternate6").hide();
+                $(".porn3").hide();
+                $(".porn4").hide();
+                $(".porn5").hide();
+                $(".porn6").hide();
                 $("#sem").change(function() {
                     var selectedValue = $(this).val();
                     if (selectedValue === "3RD") {
-                        $("#alternate3").show();
-                        $("#alternate4").hide();
-                        $("#alternate5").hide();
-                        $("#alternate6").hide();
+                        $(".porn3").show();
+                        $(".porn4").hide();
+                        $(".porn5").hide();
+                        $(".porn6").hide();
                     } else if (selectedValue === "4TH") {
-                        $("#alternate3").hide();
-                        $("#alternate4").show();
-                        $("#alternate5").hide();
-                        $("#alternate6").hide();
+                        $(".porn3").hide();
+                        $(".porn4").show();
+                        $(".porn5").hide();
+                        $(".porn6").hide();
                     } else if (selectedValue === "5TH") {
-                        $("#alternate3").hide();
-                        $("#alternate4").hide();
-                        $("#alternate5").show();
-                        $("#alternate6").hide();
+                        $(".porn3").hide();
+                        $(".porn4").hide();
+                        $(".porn5").show();
+                        $(".porn6").hide();
                     } else if (selectedValue === "6TH") {
-                        $("#alternate3").hide();
-                        $("#alternate4").hide();
-                        $("#alternate5").hide();
-                        $("#alternate6").show();
+                        $(".porn3").hide();
+                        $(".porn4").hide();
+                        $(".porn5").hide();
+                        $(".porn6").show();
                     } else {
-                        $("#alternate3").hide();
-                        $("#alternate4").hide();
-                        $("#alternate5").hide();
-                        $("#alternate6").hide();
+                        $(".porn").hide();
+                        $(".porn").hide();
+                        $(".porn").hide();
+                        $(".porn").hide();
                     }
                 });
             });
