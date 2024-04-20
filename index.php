1a7320ad88f27e765f1ee6f0b34c0ac64328d72f
Ritesh <sagarsaha@outlook.in>
2024-04-20 20:13:24 +0530

    added code tag to index.html

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<code>Changes In The True Section and Crossover all the semester parts</code>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
